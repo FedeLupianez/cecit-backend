d5125e6148a00e978570b02b72c5f2771b87a0b9
FedeLupianez <lupianezfederico@gmail.com>
2026-04-15 09:27:12 -0300

    table users complete entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -6,12 +6,28 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class UserEntity {
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn({ type: 'int', name: 'id_user' })
     id_user: number
 
-    @Column()
+    @Column({ length: 50, type: 'varchar', name: 'email' })
     email: string;
 
-    @Column({ default: true })
+    @Column({ length: 255, type: 'varchar', name: 'password' })
+    password: string;
+
+    @Column({ length: 50, type: 'varchar', name: 'name' })
+    name: string;
+
+    @Column({ length: 50, type: 'varchar', name: 'lastname' })
+    lastname: string;
+
+    @Column({ length: 10, type: 'varchar', name: 'dni' })
+    dni: string;
+
+    @Column({ type: 'date' })
+    last_activity: string;
+
+    @Column({ type: 'boolean', default: true })
     active: boolean;
+
 }
