0762e9756cdf494164000f08d06097a223ceb4a8
Matias <mngilbarbosa@gmail.com>
2026-04-20 22:03:09 -0300

    add all new files, done: .entity, .service and .dto

---
+++ src/voucher/voucher.entity.ts
@@ -0,0 +1,28 @@
+/*
+* Entidad voucher
+*/
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class VoucherEntity {
+    @PrimaryGeneratedColumn()
+    id_voucher: number
+
+    @Column()
+    id_user: number
+
+    @Column()
+    id_benefit: number
+
+    @Column()
+    token: string
+
+    @Column()
+    application_date: Date
+
+    @Column({ default: null })
+    delibery_date: Date
+
+    @Column()
+    status: Enumerator
+}
