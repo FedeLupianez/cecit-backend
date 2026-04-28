cb1f6826946a8957c66e6cfd1c644f5f43747bb9
Federico Lupiañez <lupianezfederico@gmail.com>
2026-04-28 08:02:25 -0300

    Revert "Add Benefits and BenefitTypes tables"

--- src/benefit_type/benefit_type.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('benefit-type')
-export class BenefitTypeController {}
--- src/benefit_type/benefit_type.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BenefitTypeService } from './benefit_type.service';
-import { BenefitTypeController } from './benefit_type.controller';
-
-@Module({
-  providers: [BenefitTypeService],
-  controllers: [BenefitTypeController]
-})
-export class BenefitTypeModule {}
