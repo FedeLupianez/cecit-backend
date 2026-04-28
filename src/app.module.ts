5a40744fe3f6a3c4bed86df234cc9e99821fb885
FedeLupianez <lupianezfederico@gmail.com>
2026-04-28 18:30:42 -0300

    Connect actual tables to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from './datasource/typeorm.module';
 import { ConfigModule } from '@nestjs/config';
+
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 
 import { BenefitsModule } from './benefits/benefits.module';
-import {BenefitsService} from './benefits/benefits.service';
+import { BenefitsService } from './benefits/benefits.service';
+import { BenefitsController } from './benefits/benefits.controller';
+
 import { BenefitTypeModule } from './benefit_type/benefit_type.module';
 import { BenefitTypeService } from './benefit_type/benefit_type.service';
+import { BenefitTypeController } from './benefit_type/benefit_type.controller';
 
 import { CecitAdminsModule } from './cecit-admins/cecit-admins.module';
+
+import { VoucherModule } from './voucher/voucher.module';
+import { VoucherController } from './voucher/voucher.controller';
+import { VoucherService } from './voucher/voucher.service';
+
 
 @Module({
     imports: [
@@ -22,9 +31,10 @@ import { CecitAdminsModule } from './cecit-admins/cecit-admins.module';
         UserModule,
         BenefitsModule,
         BenefitTypeModule,
-        CecitAdminsModule
+        CecitAdminsModule,
+        VoucherModule
     ],
-    controllers: [AppController, UserController],
-    providers: [AppService, UserService, BenefitTypeService, BenefitsService],
+    controllers: [AppController, UserController, VoucherController, BenefitsController, BenefitTypeController],
+    providers: [AppService, UserService, BenefitTypeService, BenefitsService, VoucherService],
 })
 export class AppModule { }
