b4d405d238f111fdaec938af24670e5c3222af3a
FedeLupianez <lupianezfederico@gmail.com>
2026-06-19 20:23:28 -0300

    add pdf creation to vouchers

--- src/entities/vouchers/vouchers.entity.ts
+++ src/entities/vouchers/vouchers.entity.ts
@@ -3,36 +3,36 @@ import { UsersEntity } from '../users/users.entity';
 import { BenefitsEntity } from '../benefits/benefits.entity';
 
 export enum VoucherStatus {
-    PENDING = 'PENDING',
-    DELIVERED = 'DELIVERED',
-    EXPIRED = 'EXPIRED'
+  PENDING = 'PENDING',
+  DELIVERED = 'DELIVERED',
+  EXPIRED = 'EXPIRED',
 }
 
 @Entity('Vouchers')
 export class VouchersEntity {
-    @PrimaryColumn({ type: 'varchar', length: 6 })
-    token !: string;
+  @PrimaryColumn({ type: 'varchar', length: 6 })
+  token!: string;
 
-    @Column({ type: 'varchar', length: 4 })
-    id_user !: string;
+  @Column({ type: 'varchar', length: 4 })
+  id_user!: string;
 
-    @ManyToOne(() => UsersEntity, { nullable: false })
-    @JoinColumn({ name: 'id_user', referencedColumnName: 'id_user' })
-    user !: UsersEntity;
+  @ManyToOne(() => UsersEntity, { nullable: false })
+  @JoinColumn({ name: 'id_user', referencedColumnName: 'id_user' })
+  user!: UsersEntity;
 
-    @Column({ type: 'varchar', length: 4 })
-    id_benefit !: string;
+  @Column({ type: 'varchar', length: 4 })
+  id_benefit!: string;
 
-    @ManyToOne(() => BenefitsEntity, { nullable: false })
-    @JoinColumn({ name: 'id_benefit', referencedColumnName: 'id_benefit' })
-    benefit !: BenefitsEntity;
+  @ManyToOne(() => BenefitsEntity, { nullable: false })
+  @JoinColumn({ name: 'id_benefit', referencedColumnName: 'id_benefit' })
+  benefit!: BenefitsEntity;
 
-    @Column({ type: 'date' })
-    application_date !: string;
+  @Column({ type: 'date' })
+  application_date!: string;
 
-    @Column({ default: null, type: 'date' })
-    delivery_date !: string;
+  @Column({ default: null, type: 'date' })
+  delivery_date!: string;
 
-    @Column({ type: 'enum', enum: VoucherStatus, default: VoucherStatus.PENDING })
-    status !: VoucherStatus;
+  @Column({ type: 'enum', enum: VoucherStatus, default: VoucherStatus.PENDING })
+  status!: VoucherStatus;
 }
